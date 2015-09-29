9313741692f4d0ebebb300cf89b598e45870fbca
jairxserfer <jairxserfer@hotmail.com>
2015-09-29 18:14:55 -0500

    initial commit

--- ACM-Trainning/266A.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-
-int main(int argc, char const *argv[]) {
-  long long n;
-  std::cin >> n;
-  std::string word;
-  std::cin >> word;
-  return 0;
-}
---
+++ ACM-Trainning/AlgoritmosNecesarios/kmp.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define MAX 50000
+using namespace std;
+int kmp_table[MAX];
+string s, t;
+void fill_table(){
+  int pos = 2, cnd = 0;
+  kmp_table[0] = -1;
+  kmp_table[1] = 0;
+  while (pos < s.size()) {
+    if (s[pos-1] == s[cnd]) {
+      kmp_table[pos++] = ++cnd;
+    } else{
+      if(cnd > 0)
+        cnd = kmp_table[cnd];
+        else
+          kmp_table[pos++] = 0;
+    }
+
+  }
+}
+
+
+int kmp(){
+  fill_table();
+  int  i = 0;
+  int m = 0;
+  while (m+i < t.size()) {
+    if(s[i] == t[m+i])
+    {
+      if(i == s.size() - 1)
+        return m;
+      i++;
+    } else{
+      m  = m + i - kmp_table[i];
+      if(kmp_table[i] > -1)
+        i =  kmp_table[i];
+      else
+        i = 0;
+    }
+  }
+  return -1;
+}
